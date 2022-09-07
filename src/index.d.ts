192f122d8541eca0282829ffb819b19e4213a078
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-07 15:32:00 -0400

    fix: update ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,10 @@
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /**
-       * Equivalent of `Cypress.Cookies.preserveOnce(name)`
-       * @example cy.preserveCookieOnce('connect.sid')
-       * @see https://github.com/bahmutov/cypress-v10-preserve-cookie
-       */
-      preserveCookieOnce(name: string): void
-    }
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Equivalent of `Cypress.Cookies.preserveOnce(name)`
+     * @example cy.preserveCookieOnce('connect.sid')
+     * @see https://github.com/bahmutov/cypress-v10-preserve-cookie
+     */
+    preserveCookieOnce(name: string): void
   }
 }
