f05385916a8778dc6752dd75b2d423fb7accd1a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-14 15:02:00 -0400

    feat: add multiple cookies

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 const debug = require('debug')('cypress-v10-preserve-cookie')
 
-Cypress.Commands.add('preserveCookieOnce', (name) => {
-  if (typeof name !== 'string' || !name) {
-    throw new Error('Expected the cookie name to preserve')
+Cypress.Commands.add('preserveCookieOnce', (...names) => {
+  if (!names.length) {
+    throw new Error('Expected at least one cookie name')
   }
-  cy.log(`preserveCookieOnce **${name}**`)
-  debug('cookie name "%s"', name)
-  const saveName = 'cookie_' + name
-  cy.getCookie(name, { log: false })
-    // disable the built-in existence check
-    .should(Cypress._.noop)
-    .then((c) => {
-      if (!c) {
-        debug('there is no cookie named %s', name)
-        const previouslySaved = Cypress.env(saveName)
-        if (previouslySaved) {
-          debug(
-            'setting the previously saved cookie %s %o',
-            name,
-            previouslySaved,
-          )
-          cy.setCookie(name, previouslySaved.value, { log: false })
+
+  names.forEach((name) => {
+    if (typeof name !== 'string' || !name) {
+      throw new Error('Expected the cookie name to preserve')
+    }
+    cy.log(`preserveCookieOnce **${name}**`)
+    debug('cookie name "%s"', name)
+    const saveName = 'cookie_' + name
+    cy.getCookie(name, { log: false })
+      // disable the built-in existence check
+      .should(Cypress._.noop)
+      .then((c) => {
+        if (!c) {
+          debug('there is no cookie named %s', name)
+          const previouslySaved = Cypress.env(saveName)
+          if (previouslySaved) {
+            debug(
+              'setting the previously saved cookie %s %o',
+              name,
+              previouslySaved,
+            )
+            cy.setCookie(name, previouslySaved.value, { log: false })
+          }
+        } else {
+          debug('saving found cookie %s %o', name, c)
+          Cypress.env(saveName, c)
         }
-      } else {
-        debug('saving found cookie %s %o', name, c)
-        Cypress.env(saveName, c)
-      }
-    })
+      })
+  })
 })
